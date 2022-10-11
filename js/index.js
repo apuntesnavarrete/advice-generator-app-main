ede9c3baf72b6eadb3ef151e0e59ed9c190a4b27
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-10-10 19:45:25 -0500

    detalles terminado

--- js/index.js
+++ js/index.js
@@ -1,26 +1,18 @@
-let num = document.getElementsByClassName("num")[0]
-let advice = document.getElementsByClassName("Advice")[0]
-let button = document.getElementById("button")
+let num = document.getElementsByClassName("num")[0];
+let advice = document.getElementsByClassName("Advice")[0];
+let button = document.getElementById("button");
 
-advice_Random()
+advice_Random();
 
-button.addEventListener("click",function(){
-   advice_Random()
-})
+button.addEventListener("click", function () {
+  advice_Random();
+});
 
-
-
-
-
-
-function advice_Random(){
-    fetch('https://api.adviceslip.com/advice')
+function advice_Random() {
+  fetch("https://api.adviceslip.com/advice")
     .then((response) => response.json())
     .then((data) => {
-      advice.innerHTML = data.slip.advice
-      num.innerHTML = data.slip.id   
+      advice.innerHTML = data.slip.advice;
+      num.innerHTML = data.slip.id;
     });
-    
 }
-
-
